9fb30701db0ae294d8ad1c9cf152b7f9b74ef8fe
Drew Showalter <maelstroswim@gmail.com>
2017-01-31 20:16:25 +1100

    Simplify / to most recent component.

--- timeout_frontend/app/app.js
+++ timeout_frontend/app/app.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 import AppDefault from './app.css';
-import RollingDigit from './components/rolling_digit';
-import DigitRoller from './components/digit_roller';
+// import RollingDigit from './components/rolling_digit';
+// import DigitRoller from './components/digit_roller';
+import YearTimer from './components/year_timer';
 
 class App extends Component {
   render() {
     return (
       <AppDefault>
-        <RollingDigit animPerSecond={1} />
-        <br />
-        <br />
-        <br />
-        <br />
-        <DigitRoller
-          min={0}
-          max={9}
-          secondPer={1}
-          startValue={5}
-        />
+        <YearTimer date={new Date()} />
       </AppDefault>
     );
   }
