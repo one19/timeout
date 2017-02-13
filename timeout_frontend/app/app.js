e7cf645c7442b87414ac332a286edb9b45680161
Drew Showalter <maelstroswim@gmail.com>
2017-02-13 21:09:07 +1100

    We can all understand ternary returns around here, right?

--- timeout_frontend/app/app.js
+++ timeout_frontend/app/app.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import AppDefault from './app.css';
-// import RollingDigit from './components/rolling_digit';
-// import DigitRoller from './components/digit_roller';
 import YearTimer from './components/year_timer';
 
 class App extends Component {
